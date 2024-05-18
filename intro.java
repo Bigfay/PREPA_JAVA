a5ff6880a6bc3351dc55cfb7c0787997c1f93d56
fay <fdraidi@hotmail.com>
2024-05-18 12:09:56 +0200

    ça avance

--- intro.java
+++ intro.java
@@ -1,7 +1,20 @@
-/**
- * intro
- */
-public class intro {
 
-    public int[] a = new int[3];
+public class intro {
+    private int a;
+    private byte b;
+    private int[] p = {3,4,5};
+    private int pint;
+
+    public intro() {
+        this.a = 3;
+        this.b = 125+1;
+        this.pint = p[2];
+    }
+
+public int getValeur() {
+    return a;
+}
+public int getPint() {
+    return pint;
+}
 }
--- lemain.java
+++ lemain.java
@@ -1,7 +1,11 @@
 
 public class lemain {
  public static void main(String[] args) {
-   int loco = a;
-   System.out.println(loco);
+   
+intro loco = new intro();
+int valeur=loco.getValeur();
+
+   System.out.println(valeur);
+   System.out.println(loco.getPint());
  }
 }
