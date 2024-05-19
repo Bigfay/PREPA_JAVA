bea63f5ac755db102a3effac89faca770105f660
fay <fdraidi@hotmail.com>
2024-05-19 08:17:10 +0200

    modify array

--- intro.java
+++ intro.java
@@ -2,19 +2,26 @@
 public class intro {
     private int a;
     private byte b;
-    private int[] p = {3,4,5};
+    private int[] p = { 3, 4, 5 };
     private int pint;
 
     public intro() {
         this.a = 3;
-        this.b = 125+1;
-        this.pint = p[2];
+        this.p[0] = 7;
+        this.b = 125 + 1;
+        this.pint = p[0];
     }
 
-public int getValeur() {
-    return a;
-}
-public int getPint() {
-    return pint;
-}
+    public int getValeur() {
+        return a;
+    }
+
+    public int getPint() {
+        return pint;
+    }
+
+    public byte getB() {
+        return b;
+    }
+
 }
